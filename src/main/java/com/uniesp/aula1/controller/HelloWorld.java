8e3fb43bd9ba40d7d9fc3350fcd829e43dc19312
thiagoZeta <95257212+thiagoZeta@users.noreply.github.com>
2022-09-11 14:35:25 -0300

    Spring boot hello world
    Spring boot hello world

---
+++ src/main/java/com/uniesp/aula1/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.uniesp.aula1.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorld {
+    @RequestMapping("/helloworld")
+    public String helloworld(){
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/uniesp/aula1/Aula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniesp.aula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
